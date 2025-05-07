4c22bd05a57465b958fb63c98e41cb11ac6f48da
SzLilla <szabolcsililla@gmail.com>
2025-05-07 10:21:21 +0200

    one test with rest assured, run manually

--- src/test/java/com/oszl/demo/DemoApplicationTests.java
+++ src/test/java/com/oszl/demo/DemoApplicationTests.java
@@ -2,12 +2,24 @@ package com.oszl.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.restassured.RestAssured;
+import io.restassured.response.*;
+import org.testng.Assert;
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetStatus() {
+		Response res = RestAssured.get("http://localhost:8080/api/employees/2");
+		int code = res.getStatusCode();
+		System.out.println("status code = " + code);
+		Assert.assertEquals(code, 200);
 	}
 
 }
